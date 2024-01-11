1e43521bb1c4e1e7a63ce89e791884f146db5164
barmandev <elbarmandev@gmail.com>
2024-01-11 14:52:55 +0100

    first commit

---
+++ src/main/java/dev/team2/zoopoli/ZoopoliApplication.java
@@ -0,0 +1,13 @@
+package dev.team2.zoopoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoopoliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoopoliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/team2/zoopoli/ZoopoliApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.team2.zoopoli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoopoliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
