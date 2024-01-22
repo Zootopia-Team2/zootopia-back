8445cd666a255ceeb9f63a1f66cded13528c3ae9
Borja <elbarmandev@gmail.com>
2024-01-22 15:25:37 +0100

    prepare merge

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO family (id_family, family) VALUES (default, 'Felids');
+INSERT INTO family (id_family, family) VALUES (default, 'Canids');
+INSERT INTO family (id_family, family) VALUES (default, 'Reptiles');
+INSERT INTO family (id_family, family) VALUES (default, 'Mustelids');
+INSERT INTO family (id_family, family) VALUES (default, 'Leporidae');
+
+INSERT INTO countries (id_country, country) VALUES (default, 'Spain');
+INSERT INTO countries (id_country, country) VALUES (default, 'Australia');
+INSERT INTO countries (id_country, country) VALUES (default, 'Africa');
+INSERT INTO countries (id_country, country) VALUES (default, 'Mexico');
+INSERT INTO countries (id_country, country) VALUES (default, 'Argentina');
+INSERT INTO countries (id_country, country) VALUES (default, 'Brasil');
